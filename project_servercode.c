9e02a83a0e62778ecaa927d614ef4475b69af19a
tam209 <62026092+tam209@users.noreply.github.com>
2020-04-05 21:35:57 -0600

    Update project_servercode.c

--- project_servercode.c
+++ project_servercode.c
@@ -1,4 +1,4 @@
-/* This code is from CS 330 lab on sockets. This code is being reused, modified, and built upon */
+/* code from CS 330 lab on sockets is being reused and modified */
 /* below is the description of the original code */
 /*****************************************************************
  Sockets Daemon Program
@@ -21,6 +21,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <pthread.h>
 
 /* Use port number 0 so that we can dynamically assign an unused
  * port number. */
@@ -33,54 +34,7 @@
 
 #define MAXLINE 1024
 
-int clientCount = 0;
 
-static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-
-struct client{
-
-  int index;
-  int sockID;
-  struct sockaddr_in clientAddr;
-  int len;
-
-};
-
-struct client Client[1024];
-pthread_t thread[1024];
-
-void * sendInfo(void * ClientDetail)
-{
-  int lf;
-  int num_char = MAXLINE;
-  char ch[MAXLINE];
-
-  struct client* clientDetail = (struct client*) ClientDetail;
-  int index = clientDetail -> index;
-  int clientSocket = clientDetail -> sockID;
-
-  printf("Client %d connected.\n",index + 1);
-
-  lf = open(LIST_FILE, O_RDONLY);
-  if (lf == -1)
-  {
-    write(clientSocket, "No options.\n", strlen("No options.\n"));
-  }
-  /* Read from file, write to socket */
-  else
-  {
-    while ((num_char = read(lf, ch, MAXLINE)) > 0) 
-    {
-      if (write(clientSocket, ch, num_char) < num_char)
-      {
-        OOPS("Writing");
-      }
-    }
-    close(lf);
-  }
-  close(clientSocket);
-}
 
 int main()
 {
@@ -137,33 +91,53 @@ int main()
   getsockname(s,(struct sockaddr *)&saddr,&slen);
   printf("Socket assigned: %d\n",ntohs(saddr.sin_port));
 
-  /* Tell socket to wait for input.  Queue length is 1. */
-  if(listen(s,1) != 0)
+  /* Tell socket to wait for input.  Queue length is 5. */
+  if(listen(s,5) != 0)
     OOPS("listen");
+
+
+  /* accept */
+  sfd = accept(s, NULL, NULL);
+  if (sfd == -1)
+    OOPS("accept");
   
   /* Loop indefinately and wait for events. */
   for(;;)
   {
-    /* Wait in the 'accept()' call for a client to make a connection. */
-    Client[clientCount].sockID = accept(s, (struct sockaddr*) &Client[clientCount].clientAddr, &Client[clientCount].len);
-    if (Client[clientCount].sockID == -1)
+
+    /* Open file containing list options */
+    lf = open(LIST_FILE, O_RDONLY);
+    if (lf == -1)
     {
-      OOPS("accept");
+      write(sfd, "No options.\n", strlen("No options.\n"));
     }
-    
-    Client[clientCount].index = clientCount;
 
-    pthread_create(&thread[clientCount], NULL, sendInfo, (void *) &Client[clientCount]);
-    
-    clientCount++;
-  }
+    //Read from the list file and write to the socket
+    while ((num_char = read(lf, ch, MAXLINE)) > 0) 
+    {
+      if (write(sfd, ch, num_char) < num_char)
+      {
+       OOPS("Writing");
+      }
+    }
+    close(lf);
 
-  int i = 0;
-  while (i < clientCount)
-  {
-    pthread_join(thread[i], NULL);
-    i++;
+    bzero(ch, sizeof(ch));
+
+    //Read from the socket
+    read(sfd, ch, sizeof(ch));
+
+
+    if (strncmp("1", ch, 1) == 0)
+    {
+      printf("This is option 1\n");
+      break;
+    }
+
   }
+  close(s);
+
+
     
   return 0;
 }
