ef3b53e615a24410262ef9493fbc523a97b8e215
tam209 <62026092+tam209@users.noreply.github.com>
2020-04-08 11:18:56 -0600

    Update project_servercode.c
    Added threading to code to allow multiple clients to be able to access the server at the same time.
    Also working on the implementation of the chat function.
    Code used for threading is credit to Abhey Rana, Multiple client chat system, https://gist.github.com/Abhey/47e09377a527acfc2480dbc5515df872

--- project_servercode.c
+++ project_servercode.c
@@ -1,5 +1,10 @@
-/* code from CS 330 lab on sockets is being reused and modified */
+/* code from CS 330 lab on sockets is being reused and modified - most of the socket setup code is 
+   from the CS 330 lab code used in the exercise
+*/
 /* below is the description of the original code */
+/* code on threading is credit to https://gist.github.com/Abhey/47e09377a527acfc2480dbc5515df872
+   Abhey Rana, Multiple Client Chat Description [Source Code]
+*/
 /*****************************************************************
  Sockets Daemon Program
 
@@ -37,7 +42,187 @@
 
 #define MAXLINE 1024
 
+/* 
+Title: Multiple Client Chat Description
+Author: Abhey Rana
+https://gist.github.com/Abhey/47e09377a527acfc2480dbc5515df872
+*/
+//initialize the clientCount
+int clientCount = 0;
 
+struct client_info
+{
+ int index;
+ int sockID;
+ struct sockaddr_in clientAddr;
+ int length;
+};
+
+struct client_info Client[1024];
+pthread_t thread[1024];
+
+void *sendInfo(void * client_detail)
+{
+ int lf; //file descriptor for list.bbs.file
+ int num_char = MAXLINE; 
+ char ch[MAXLINE]; //character for i/o
+ 
+ //enter the client info that is being passed into a struct called clientDetail
+ struct client_info* clientDetail = (struct client_info*) client_detail;
+ int index = clientDetail -> index;
+ int clientSocket = clientDetail -> sockID;
+ 
+ printf("Client %d connected.\n", index + 1);
+ 
+ for(;;)
+  {
+
+    /* Open file containing list options */
+    lf = open(LIST_FILE, O_RDONLY);
+    if (lf == -1)
+    {
+      write(clientSocket, "No options.\n", strlen("No options.\n"));
+    }
+
+    //Read from the list file and write to the socket
+    while ((num_char = read(lf, ch, MAXLINE)) > 0) 
+    {
+      if (write(clientSocket, ch, num_char) < num_char)
+      {
+       OOPS("Writing");
+      }
+    }
+    close(lf);
+
+    //erase the data in ch
+    bzero(ch, sizeof(ch));
+
+    //Read from the socket
+    read(clientSocket, ch, sizeof(ch));
+
+    //if user enters "1", open HTML file, write HTML file to socket
+    if (strncmp("1", ch, 1) == 0)
+    {
+      //erase the data in ch
+      bzero(ch, sizeof(ch));
+      int htmlf;       /* file descriptor for html file */
+
+      htmlf = open(HTML_FILE, O_RDONLY);
+      if (htmlf == -1)
+      {
+        write(clientSocket, "No HTML file.\n", strlen("No HTML file.\n"));
+      }
+
+      printf("Sending HTML file to Client %d...\n", index + 1);
+
+      while ((num_char = read(htmlf, ch, MAXLINE)) > 0) 
+      {
+        if (write(clientSocket, ch, num_char) < num_char)
+        {
+          OOPS("Writing");
+        }
+      }
+      close(htmlf);
+      break;
+    }
+
+    //else if user enters "2", open CSS file, write CSS file to socket
+    else if (strncmp("2", ch, 1) == 0)
+    {
+      //erase the data in ch
+      bzero(ch, sizeof(ch));
+      int cssf;       /* file descriptor for css file */
+
+      cssf = open(CSS_FILE, O_RDONLY);
+      if (cssf == -1)
+      {
+        write(clientSocket, "No CSS file.\n", strlen("No CSS file.\n"));
+      }
+
+      printf("Sending CSS file to Client %d...\n", index + 1);
+
+      while ((num_char = read(cssf, ch, MAXLINE)) > 0) 
+      {
+        if (write(clientSocket, ch, num_char) < num_char)
+        {
+          OOPS("Writing");
+        }
+      }
+      close(cssf);
+      break;
+    }
+
+    //else if user enters "3", open JS file, write JS file to socket
+    else if (strncmp("3", ch, 1) == 0)
+    {
+      //erase the data in ch
+      bzero(ch, sizeof(ch));
+      int jsf;       /* file descriptor for js file */
+
+      jsf = open(JS_FILE, O_RDONLY);
+      if (jsf == -1)
+      {
+        write(clientSocket, "No JS file.\n", strlen("No JS file.\n"));
+      }
+
+      printf("Sending JS file to Client %d...\n", index + 1);
+
+      while ((num_char = read(jsf, ch, MAXLINE)) > 0) 
+      {
+        if (write(clientSocket, ch, num_char) < num_char)
+        {
+          OOPS("Writing");
+        }
+      }
+      close(jsf);
+      break;
+    }
+
+    //else if user enters "chat"
+    else if (strncmp("chat", ch, 4) == 0)
+    {
+      for (;;)
+      {
+        //erase the data in ch
+        bzero(ch, sizeof(ch));
+
+        //read from client socket
+        read(clientSocket, ch, sizeof(ch));
+
+        //write to all other clients
+        int i = 1;
+        while (i < clientCount)
+        {
+          if (i != index)
+          {
+            write(Client[i].sockID, ch, sizeof(ch));
+          }
+        }
+      }
+    }
+
+    //else if user enters "exit", client has closed the session
+    else if (strncmp("exit", ch, 4) == 0)
+    {
+      printf("Client %d has exited the session....\n", index + 1);
+      break;
+    }
+
+    //If user enters an invalid response. Nothing happens to server
+    else
+    {
+      printf("\n");
+    }
+
+  }
+
+  close(clientSocket);
+}
+
+
+/*
+CS330 Lab Sockets
+*/
 
 int main()
 {
@@ -103,122 +288,35 @@ int main()
   if (sfd == -1)
     OOPS("accept");
   
+
+/* 
+Title: Multiple Client Chat Description
+Author: Abhey Rana
+https://gist.github.com/Abhey/47e09377a527acfc2480dbc5515df872
+*/
   /* Loop indefinately and wait for events. */
   for(;;)
-  {
-
-    /* Open file containing list options */
-    lf = open(LIST_FILE, O_RDONLY);
-    if (lf == -1)
     {
-      write(sfd, "No options.\n", strlen("No options.\n"));
+    /* Wait in the 'accept()' call for a client to make a connection. */
+    Client[clientCount].sockID = accept(s, (struct sockaddr*) &Client[clientCount].clientAddr, &Client[clientCount].len);
+    if (Client[clientCount].sockID == -1)
+    {
+      OOPS("accept");
     }
 
-    //Read from the list file and write to the socket
-    while ((num_char = read(lf, ch, MAXLINE)) > 0) 
-    {
-      if (write(sfd, ch, num_char) < num_char)
-      {
-       OOPS("Writing");
-      }
-    }
-    close(lf);
+    Client[clientCount].index = clientCount;
 
-    bzero(ch, sizeof(ch));
-
-    //Read from the socket
-    read(sfd, ch, sizeof(ch));
-
-
-    //if user enters "1", open HTML file, write HTML file to socket
-    if (strncmp("1", ch, 1) == 0)
-    {
-      bzero(ch, sizeof(ch));
-      int htmlf;       /* file descriptor for html file */
-
-      htmlf = open(HTML_FILE, O_RDONLY);
-      if (htmlf == -1)
-      {
-        write(sfd, "No HTML file.\n", strlen("No HTML file.\n"));
-      }
-
-      printf("Sending HTML file...\n");
-
-      while ((num_char = read(htmlf, ch, MAXLINE)) > 0) 
-      {
-        if (write(sfd, ch, num_char) < num_char)
-        {
-          OOPS("Writing");
-        }
-      }
-      close(htmlf);
-      break;
-    }
-
-    //else if user enters "2", open CSS file, write CSS file to socket
-    else if (strncmp("2", ch, 1) == 0)
-    {
-      bzero(ch, sizeof(ch));
-      int cssf;       /* file descriptor for css file */
-
-      cssf = open(CSS_FILE, O_RDONLY);
-      if (htmlf == -1)
-      {
-        write(sfd, "No CSS file.\n", strlen("No CSS file.\n"));
-      }
-
-      printf("Sending CSS file...\n");
-
-      while ((num_char = read(cssf, ch, MAXLINE)) > 0) 
-      {
-        if (write(sfd, ch, num_char) < num_char)
-        {
-          OOPS("Writing");
-        }
-      }
-      close(cssf);
-      break;
-    }
-
-    //else if user enters "3", open JS file, write JS file to socket
-    else if (strncmp("3", ch, 1) == 0)
-    {
-      bzero(ch, sizeof(ch));
-      int jsf;       /* file descriptor for js file */
-
-      jsf = open(JS_FILE, O_RDONLY);
-      if (htmlf == -1)
-      {
-        write(sfd, "No JS file.\n", strlen("No JS file.\n"));
-      }
-
-      printf("Sending JS file...\n");
-
-      while ((num_char = read(jsf, ch, MAXLINE)) > 0) 
-      {
-        if (write(sfd, ch, num_char) < num_char)
-        {
-          OOPS("Writing");
-        }
-      }
-      close(jsf);
-      break;
-    }
-   
-    //else if user enters "exit", client has closed the session
-    else if (strncmp("exit", ch, 4) == 0)
-    {
-    	 printf("Client has exited the session....\n");
-    	 break;
-    }
-    //If user enters an invalid response. Nothing happens to server
-    else
-    {
-      printf("\n");
-    }
-
+    pthread_create(&thread[clientCount], NULL, sendInfo, (void *) &Client[clientCount]);
+    
+    clientCount++;
   }
-  close(s);
+
+  int i = 0;
+  while (i < clientCount)
+  {
+    pthread_join(thread[i], NULL);
+    i++;
+  }
 
 
     
